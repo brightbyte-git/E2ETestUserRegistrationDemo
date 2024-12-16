f4be2d673e5eb0a6e4d558507764cf1c9a522222
kieron-cairns <kieroncairns@protonmail.com>
2024-12-16 21:06:55 +0000

    CHANGE: User registration API endpoint modified and frontend.zip file removed.

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5182/api";
+const BASE_URL = "http://localhost:5003/api";
 
 export const registerUser = async (userData) => {
     return await axios.post(`${BASE_URL}/User/register`, userData);
