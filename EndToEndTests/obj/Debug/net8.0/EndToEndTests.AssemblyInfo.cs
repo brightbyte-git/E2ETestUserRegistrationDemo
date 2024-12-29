44716f0acf7fd94301455c269baba6ac723da011
kieron-cairns <kieroncairns@protonmail.com>
2024-12-29 10:32:02 +0000

    CHANGE:

--- EndToEndTests/obj/Debug/net8.0/EndToEndTests.AssemblyInfo.cs
+++ EndToEndTests/obj/Debug/net8.0/EndToEndTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EndToEndTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7d9ba4596cf1640a2c1b7257ba9fbc0987e31f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5acb21bf165b8a32b20f5136e4157e2e19f9e9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("EndToEndTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EndToEndTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
